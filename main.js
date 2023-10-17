c4bf0623f5417c38f266584b1c0cddaf457e319e
DamianoL <damianolunardo@hotmail.it>
2023-10-17 18:28:30 +0200

    ADD input user numbers and comparison with pc

--- main.js
+++ main.js
@@ -10,35 +10,59 @@ Descrizione:
 function randomNumbers() {
     let casualNumbers = [];
     for (let i = 0; i < 5; i++) {
-        casualNumbers.push(Math.floor(Math.random() * 100) + 1); 
+        casualNumbers.push(Math.floor(Math.random() * 100) + 1);
     }
     return casualNumbers;
-}
+};
 let generatedNumbers = randomNumbers();
 alert("Ecco i 5 numeri casuali: " + generatedNumbers);
 
 
 //aggiungo il timer di 30 secondi
-let timeLeft = 30;
+//let timeLeft = 30;
+let timeLeft = 5; // per test più veloce
 let elem = document.getElementById('Timer');
 let timerId = setInterval(countdown, 1000);
 
 function countdown() {
-  if (timeLeft == 0) {
-    clearTimeout(timerId);
-    
-  } else {
-    elem.innerHTML = timeLeft + ' secondi rimanenti';
-    timeLeft--;
-  }
-}
+    if (timeLeft == 0) {
+        clearTimeout(timerId);
+        getUserInput();
+    } else {
+        elem.innerHTML = timeLeft + ' secondi rimanenti';
+        timeLeft--;
+    }
+};
+
+// devo prende l'imput dell'utente
+function getUserInput() {
+    let userNumbers = [];
+    for (let i = 0; i < 5; i++) {
+        let userInput = prompt("Inserisci un numero");
+        userNumbers.push(parseInt(userInput));
+    }
+
+    // devo controllare che i numeri inseriti dall'utente siano uguali a quelli generati dal computer
+    let equalNumbers = [];
+    for (let i = 0; i < 5; i++) {
+        if (userNumbers[i] === generatedNumbers[i]) {
+            equalNumbers.push(userNumbers[i]);
+        }
+    };
+
+    // ora devo stampare i risultati e confrontarli per poterli inserire
+    let finalMessage = "Corretto" + equalNumbers.length + " numeri su 5";
+    if (equalNumbers.length > 0) {
+        finalMessage = "I numeri giusti sono: " + equalNumbers;
+    } else {
+        finalMessage = "Tutti i numeri sono sbagliati";
+    }
+    alert(finalMessage);
+};
 
 
 
-//funzione per gestire il tempo
 
-//dopo 30 secondo utente scrive i numeri che si ricorda
-//dire quali e quanti numeri utente ha inserito correttamente
 
 
 
